b9e6aa01dcde722005992296f0795273337eff85
Brice Marcel Kouadio <brice.kouadio@inexa-ci.com>
2023-04-27 12:29:25 +0000

    TokenService

---
+++ ConsumeDataLabFT/Services/TokenService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumeDataLabFT.Services
+{
+    public class TokenService
+    {
+        private readonly ServicesHttpClient servicesHttpClient;
+        public TokenService()
+        {
+            servicesHttpClient = ServicesHttpClient.GetInstance;
+        }
+
+        public string GetToken()
+        {
+            servicesHttpClient.ServicesHttpClientBaseAddress
+        }
+    }
+}
