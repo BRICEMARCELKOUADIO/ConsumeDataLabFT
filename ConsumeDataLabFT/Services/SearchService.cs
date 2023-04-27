5ce179e94f9dd70d89d605fe554f6eab76942ed8
Brice Marcel Kouadio <brice.kouadio@inexa-ci.com>
2023-04-27 12:00:32 +0000

    Search Service

---
+++ ConsumeDataLabFT/Services/SearchService.cs
@@ -0,0 +1,37 @@
+﻿using ConsumeDataLabFT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumeDataLabFT.Services
+{
+    public class SearchService : IProviderWebService
+    {
+        public Task<bool> DeleteAsync<T>(string Url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync<T>(string Url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> PostAsync<T>(string Url, T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<R> PostAsyncComplex<R, T>(string Url, T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> PutAsyn<T>(string Url, T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
